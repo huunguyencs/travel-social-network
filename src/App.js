fc1654dbc257c98d961bfdea8536ed98ab7b6767
Your Name <you@example.com>
2021-10-27 20:23:37 +0700

    login-register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import HomePage from "./page/HomePage";
-
-
+import './App.css';
+import Login from "./page/login/login";
+import  Register from "./page/login/register";
 function App() {
   return (
-    <HomePage></HomePage>
+    // <HomePage></HomePage>
+    <Login></Login>
+    // <Register></Register>
   );
 }
 
---
+++ src/page/message/message.js
@@ -0,0 +1,19 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+
+import React from "react";
+
+import TextField from '@material-ui/core/TextField/TextField';
+import Button from '@material-ui/core/Button/Button';
+
+
+const useStyles = makeStyles((theme) => ({
+    
+}));
+
+export default function  Message(props) {
+    const classes = useStyles();
+
+    return (
+        <div className=""></div>
+    )
+}
