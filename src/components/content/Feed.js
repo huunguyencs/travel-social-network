035df19099a3ff478496b7de0cf43b605591287a
Huu Nguyen <thieuvan0405@gmail.com>
2021-10-27 20:24:14 +0700

    fix slider

--- src/components/content/Feed.js
+++ src/components/content/Feed.js
@@ -4,6 +4,118 @@ import Post from "./Post";
 import React, { useState } from "react";
 import { AddCircleOutline, Create } from "@material-ui/icons";
 
+const listPost = [
+    {
+        user: {
+            avatar: "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392",
+            name: "Trần Văn A",
+        },
+        time: "26 Thg 10 2021",
+        imgList: [
+            {
+                img: "https://img.thuthuatphanmem.vn/uploads/2018/10/26/anh-dep-cau-rong-da-nang-viet-nam_055418962.jpg",
+                title: "img1",
+            },
+            {
+                img: "https://static.dalaco.travel/intranet/images/thoi-gian-cau-rong-phun-lua-da-nang.jpg",
+                title: "img2",
+            }
+        ],
+        content: "Đây là content. Tôi đang thấy vui ...",
+        numLike: 12,
+        liked: true,
+        cmts: [
+            {
+                user: {
+                    avatar: "",
+                    name: "Nguyễn Văn C"
+                },
+                content: "Đây là comment",
+                time: "27 Thg 10 2021",
+                numLike: 1,
+                liked: false,
+            },
+            {
+                user: {
+                    avatar: "",
+                    name: "Nguyễn Văn D"
+                },
+                content: "Comment",
+                time: "26 Thg 10 2021",
+                numLike: 2,
+                liked: true,
+            }
+        ]
+    },
+    {
+        user: {
+            avatar: "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392",
+            name: "Trần Văn B",
+        },
+        time: "27 Thg 10 2021",
+        imgList: [
+            {
+                img: "https://toptour.com.vn/wp-content/uploads/2019/08/nhung-dia-diem-dep1.jpg",
+                title: "img1",
+            },
+            {
+                img: "https://znews-photo.zadn.vn/w660/Uploaded/ngtmns/2016_06_24/1.jpg",
+                title: "img2",
+            },
+            {
+                img: "https://znews-photo.zadn.vn/w660/Uploaded/ngtmns/2016_06_24/1.jpg",
+                title: "img3",
+            }
+        ],
+        content: "Cảnh đẹp Việt Nam",
+        numLike: 10,
+        liked: false,
+        cmts: [],
+    },
+    {
+        user: {
+            avatar: "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392",
+            name: "Phạm C",
+        },
+        time: "26 Thg 10 2021",
+        imgList: [
+            {
+                img: "https://img.thuthuatphanmem.vn/uploads/2018/10/26/anh-dep-cau-rong-da-nang-viet-nam_055418962.jpg",
+                title: "img1",
+            },
+            // {
+            //     img: "https://static.dalaco.travel/intranet/images/thoi-gian-cau-rong-phun-lua-da-nang.jpg",
+            //     title: "img2",
+            // }
+        ],
+        content: "Đây là content. Tôi đang thấy vui ...",
+        numLike: 5,
+        liked: true,
+        cmts: [
+            {
+                user: {
+                    avatar: "",
+                    name: "Nguyễn Văn C"
+                },
+                content: "Đây là comment",
+                time: "27 Thg 10 2021",
+                numLike: 1,
+                liked: false,
+            },
+            {
+                user: {
+                    avatar: "",
+                    name: "Phạm C"
+                },
+                content: "Comment",
+                time: "28 Thg 10 2021",
+                numLike: 0,
+                liked: false,
+            }
+        ]
+    }
+]
+
 const useStyles = makeStyles((theme) => ({
     container: {
         marginTop: theme.spacing(10),
@@ -58,72 +170,7 @@ export default function Feed(props) {
 
     const classes = useStyles({ show });
 
-    const listPost = [
-        {
-            user: {
-                avatar: "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392",
-                name: "Trần Văn A",
-            },
-            time: "October 26, 2021",
-            imgList: [
-                {
-                    img: "https://img.thuthuatphanmem.vn/uploads/2018/10/26/anh-dep-cau-rong-da-nang-viet-nam_055418962.jpg",
-                    title: "img1",
-                },
-                {
-                    img: "https://static.dalaco.travel/intranet/images/thoi-gian-cau-rong-phun-lua-da-nang.jpg",
-                    title: "img2",
-                }
-            ],
-            content: "Đây là content. Tôi đang thấy vui ...",
-            numLike: 12,
-            liked: true,
-            cmts: [
-                {
-                    user: {
-                        avatar: "",
-                        name: "Nguyễn Văn C"
-                    },
-                    content: "Đây là comment",
-                    time: "October 26, 2021",
-                    numLike: 1,
-                    liked: false,
-                },
-                {
-                    user: {
-                        avatar: "",
-                        name: "Nguyễn Văn D"
-                    },
-                    content: "Comment",
-                    time: "October 26, 2021",
-                    numLike: 2,
-                    liked: true,
-                }
-            ]
-        },
-        {
-            user: {
-                avatar: "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392",
-                name: "Trần Văn B",
-            },
-            time: "October 27, 2021",
-            imgList: [
-                {
-                    img: "https://toptour.com.vn/wp-content/uploads/2019/08/nhung-dia-diem-dep1.jpg",
-                    title: "img1",
-                },
-                {
-                    img: "https://znews-photo.zadn.vn/w660/Uploaded/ngtmns/2016_06_24/1.jpg",
-                    title: "img2",
-                }
-            ],
-            content: "Cảnh đẹp Việt Nam",
-            numLike: 10,
-            liked: false,
-            cmts: [],
-        },
 
-    ]
 
     return (
         <Container className={classes.container}>
