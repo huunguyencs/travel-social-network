b6874990929f03e105c773b8d705046f167ed2c5
Your Name <you@example.com>
2021-11-04 16:36:47 +0700

    list follow

--- src/components/Profile/avatar.js
+++ src/components/Profile/avatar.js
@@ -1,15 +1,72 @@
 import React from "react";
-import { Home, WhatsApp, } from "@material-ui/icons";
-import { Avatar, Button, Container, Typography } from "@material-ui/core";
+import { Home, WhatsApp,Cancel } from "@material-ui/icons";
+import { Avatar, Button, Container, Typography, Modal, Backdrop,List, ListItem, ListItemText, ListItemAvatar, ListItemSecondaryAction } from "@material-ui/core";
 
-import profileStyles from '../../style';
-import { Link } from "react-router-dom";
+import { profileStyles } from "../../style";
+
 
 
 
 
 export default function Profile_Avatar(props) {
   const classes = profileStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const body = (
+    <div className={classes.paper}>
+      <div className={classes.modal_header}>
+          <h2  className={classes.modal_header_left}>Following</h2>
+        <div className={classes.modal_header_right}>
+          <Cancel className={classes.modal_header_closeIcon} />
+        </div>
+      </div>
+      <List className={classes.modal_body}>
+        <ListItem button className={classes.modal_body_user}>
+            <ListItemAvatar>
+                <Avatar alt="avatar" src="">
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Tran Van An" />
+            <ListItemSecondaryAction> 
+              <Button variant="outlined"className={classes.modal_body_user_button}  >
+                Follow
+              </Button>
+            </ListItemSecondaryAction>
+        </ListItem>
+        <ListItem button className={classes.modal_body_user}>
+            <ListItemAvatar>
+                <Avatar alt="avatar" src="">
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Tran Van Be" />
+            <ListItemSecondaryAction> 
+              <Button variant="outlined"className={classes.modal_body_user_button}  >
+                Follow
+              </Button>
+            </ListItemSecondaryAction>
+        </ListItem>
+        <ListItem button className={classes.modal_body_user}>
+            <ListItemAvatar>
+                <Avatar alt="avatar" src="">
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Tran Van Be" />
+            <ListItemSecondaryAction> 
+              <Button variant="outlined"className={classes.modal_body_user_button}  >
+                Follow
+              </Button>
+            </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+    </div>
+  );
   return (
     <Container className={classes.container}>
       <div>
@@ -24,9 +81,23 @@ export default function Profile_Avatar(props) {
             Tran Van A
           </Typography>
           <div >
-            <Typography variant="body1" color="#9b9696" component="p" style={{ fontSize: "20px", }}>
-              <Link style={{ marginRight: "20px", }}>30 Following</Link>
-              <Link >30 Followed</Link>
+            <Typography variant="body1" color="#9b9696" component="p" style={{ display:"flex",fontSize: "20px", }}>
+              <Typography style={{ marginRight: "20px",cursor:"pointer", }} onClick={handleOpen} >30 Following</Typography>
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                  {body}
+              </Modal>
+              <Typography style={{ cursor:"pointer", }} onClick={handleOpen} >30 Followed</Typography>
             </Typography>
           </div>
         </div>
