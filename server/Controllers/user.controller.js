9438cd51a690cb49a0d8d2cbe131cc26d49383ab
Huu Nguyen <thieuvan0405@gmail.com>
2021-11-18 15:45:54 +0700

    merge huy

--- server/Controllers/user.controller.js
+++ server/Controllers/user.controller.js
@@ -1,34 +1,36 @@
-const Users  = require('../Models/user.model');
+const Users = require('../Models/user.model');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
 
 class UserController {
-    async register(req, res){
-        try{
-            const {fullname, username, email, phone, gender, password} = req.body;
+    async register(req, res) {
+        try {
+            const { fullname, username, email, phone, gender, password } = req.body;
 
+            // khong can
             //simple validation
-            if( !username || !password || !fullname || !email || !phone || !gender ){
-                return res.status(400).json({success: false, message:"Missing field"})
+            if (!username || !password || !fullname || !email || !phone || !gender) {
+                return res.status(400).json({ success: false, message: "Missing field" })
             }
-            const user_name = await Users.findOne({username})
-            if(user_name) return res.status(400).json({success:false, message:"This user name already exists."})
+            const user_name = await Users.findOne({ username })
+            if (user_name) return res.status(400).json({ success: false, message: "This user name already exists." })
 
-            const user_email = await Users.findOne({email})
-            if(user_email) return res.status(400).json({success:false, message:"This email already exists"})
+            const user_email = await Users.findOne({ email })
+            if (user_email) return res.status(400).json({ success: false, message: "This email already exists" })
 
+            // khong can
             if (password.length < 6)
-                return res.status(400).json({ success:false, message: "Password must be at least 6 characters." })
-            
+                return res.status(400).json({ success: false, message: "Password must be at least 6 characters." })
+
             const passwordHash = await bcrypt.hash(password, 12)
 
             const userNew = new Users({
                 fullname, username, email, password: passwordHash, phone, gender
             })
 
-            
+
             //Return Token
-            const accessToken = jwt.sign({ id: userNew._id },process.env.ACCESS_TOKEN_SECRET|| "abcdefghiklmn")
+            const accessToken = jwt.sign({ id: userNew._id }, process.env.ACCESS_TOKEN_SECRET || "abcdefghiklmn")
 
             //Cookie
             // const refresh_token = createRefreshToken({ id: newUser._id })
@@ -43,32 +45,32 @@ class UserController {
 
             res.json({
                 success: true,
-                message:"register successful!",
+                message: "register successful!",
                 accessToken
             })
-        }catch(err){
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success: false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
 
-    async login(req, res){
-        try{
-            const {email, password} = req.body
-            
+    async login(req, res) {
+        try {
+            const { email, password } = req.body
+
             //simple validation
-            if( !password || !email){
-                return res.status(400).json({success: false, message:"Missing field password or email"})
+            if (!password || !email) {
+                return res.status(400).json({ success: false, message: "Missing field password or email" })
             }
-        
-            const user = await Users.findOne({email}).populate("followers followings", "username avatar fullname followers followings")
-            if(!user) return res.status(400).json({success: false,  message:"email incorrect"})
+
+            const user = await Users.findOne({ email }).populate("followers followings", "username avatar fullname followers followings")
+            if (!user) return res.status(400).json({ success: false, message: "email incorrect" })
             const passwordValid = await bcrypt.compare(password, user.password)
-            if(!passwordValid) return res.status(400).json({success: false, message:"password incorrect"})
-            
+            if (!passwordValid) return res.status(400).json({ success: false, message: "password incorrect" })
+
             //all Good
             //Return Token
-            const accessToken = jwt.sign({ id: user._id },process.env.ACCESS_TOKEN_SECRET|| "abcdefghiklmn")
+            const accessToken = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET || "abcdefghiklmn")
             //trả về dữ liệu user khi login thành công
 
             // const refresh_token = createRefreshToken({ id: user._id })
@@ -80,40 +82,40 @@ class UserController {
             // })
             res.json({
                 success: true,
-                message:"login successful!",
+                message: "login successful!",
                 accessToken,
-                user:{
+                user: {
                     ...user._doc,
                     password: ''
                 }
             })
 
-        }catch(err){
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
 
-    async logout(req, res){
-        try{
+    async logout(req, res) {
+        try {
             // res.clearCookie('refreshtoken', { path: '/api/refresh_token' })
-            return res.json({success:true,message: "logout successful!"})
-        }catch(err){
+            return res.json({ success: true, message: "logout successful!" })
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
 
-    async changeAvatar(req, res){
-        try{
-            const {avatar} = req.body
+    async changeAvatar(req, res) {
+        try {
+            const { avatar } = req.body
             await Users.findByIdAndUpdate(req.user._id, {
                 avatar
             })
-            res.json({success:true, message: "Update avatar successful"})
-        }catch(err){
+            res.json({ success: true, message: "Update avatar successful" })
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
     async changeBackground(req, res) {
@@ -122,45 +124,45 @@ class UserController {
             await Users.findByIdAndUpdate(req.user._id, {
                 background
             })
-            res.json({success:true, message: "Update background successful"})
+            res.json({ success: true, message: "Update background successful" })
         } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
     async editProfile(req, res) {
         try {
-            const { fullname, phone, address,birthday,hobbies, gender } = req.body
+            const { fullname, phone, address, birthday, hobbies, gender } = req.body
             await Users.findByIdAndUpdate(req.user._id, {
-                fullname, phone, address,birthday,hobbies, gender
+                fullname, phone, address, birthday, hobbies, gender
             })
-            res.json({success:true, message: "Update profile successful"})
+            res.json({ success: true, message: "Update profile successful" })
         } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
-    
-    async getUser(req, res){
-        try{
+
+    async getUser(req, res) {
+        try {
             const user = await Users.findById(req.params.id)
                 .populate("followers followings", "-password")
-            if (!user) return res.status(400).json({success:false, massage: "User does not exist." })
-            res.json({success:true , user })
-           
-        }catch(err){
+            if (!user) return res.status(400).json({ success: false, massage: "User does not exist." })
+            res.json({ success: true, user })
+
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
 
     // A(user._id) follow B(params.id)
-    async  follow(req, res){
-        try{
-           
-            const user = await Users.find({_id: req.params.id, followers: req.user._id})
+    async follow(req, res) {
+        try {
+
+            const user = await Users.find({ _id: req.params.id, followers: req.user._id })
             if (user.length > 0) {
-                return res.status(400).json({success: false, message: "You follwed this user." });
+                return res.status(400).json({ success: false, message: "You follwed this user." });
             }
             //cập nhập ds follower ở B
             await Users.findOneAndUpdate({ _id: req.params.id }, {
@@ -172,16 +174,16 @@ class UserController {
                 $push: { followings: req.params.id }
             }, { new: true })
 
-            res.json({success: true, message:"follow successful"})
-        }catch(err){
+            res.json({ success: true, message: "follow successful" })
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
 
     }
     // A(user._id) unfollow B(params.id)
-    async  unfollow(req, res){
-        try{
+    async unfollow(req, res) {
+        try {
             // cập nhập ds ở B
             await Users.findOneAndUpdate({ _id: req.params.id }, {
                 $pull: { followers: req.user._id }
@@ -192,25 +194,25 @@ class UserController {
                 $pull: { followings: req.params.id }
             }, { new: true })
 
-            res.json({success: true, message:"unfollow successful"})
-        }catch(err){
+            res.json({ success: true, message: "unfollow successful" })
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
 
     }
 
-    async searchUsers(req, res){
-        try{
+    async searchUsers(req, res) {
+        try {
             // console.log("====", req.query.fullname)
             // res.json("ok")
             const users = await Users.find({ fullname: { $regex: req.query.fullname } })
                 .limit(10).select("fullname avatar");
 
-            res.json({success: true,users})
-        }catch(err){
+            res.json({ success: true, users })
+        } catch (err) {
             console.log(err)
-            res.status(500).json({success:false, message: err.message})
+            res.status(500).json({ success: false, message: err.message })
         }
     }
 }
