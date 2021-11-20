001a5552416b2b3a5cb74ec32c3a315e2317ee71
Huu Nguyen <thieuvan0405@gmail.com>
2021-11-20 12:14:38 +0700

    remove see more menu

--- src/components/leftbar/menu.js
+++ src/components/leftbar/menu.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import {
     ListItem,
     List,
     ListItemIcon,
     ListItemText,
-    Collapse
+    // Collapse
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { ExpandLess, ExpandMore } from "@material-ui/icons";
+// import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import { useLocation } from "react-router-dom";
 
 import { leftbarStyles } from "../../style";
@@ -16,9 +16,9 @@ export default function Menu(props) {
 
     const location = useLocation();
 
-    const [more, setMore] = useState(false);
+    // const [more, setMore] = useState(false);
 
-    const classes = leftbarStyles({ more });
+    const classes = leftbarStyles();
 
     const isActive = (path) => {
         // if (path === "/" && location.pathname === "/home") return true;
@@ -41,13 +41,13 @@ export default function Menu(props) {
     }
 
     const menuList = props.menuList;
-    const hasMoreBtn = menuList.menu.length > 4 ? true : false;
+    // const hasMoreBtn = menuList.menu.length > 4 ? true : false;
 
     return (
 
 
         <List className={classes.panel}>
-            {menuList.menu.slice(0, 4).map((item, index) => (
+            {menuList.menu.map((item, index) => (
                 <Link style={{ textDecoration: "none", }} to={renderLink(item.link)} key={index}>
                     <ListItem button className={isActive(item.link) ? [classes.item, classes.itemActive] : classes.item}>
                         <ListItemIcon>
@@ -60,36 +60,6 @@ export default function Menu(props) {
                     </ListItem>
                 </Link>
             ))}
-            {
-                hasMoreBtn && (
-                    <>
-                        <Collapse in={more}>
-                            {menuList.menu.slice(4).map((item, index) => (
-                                <Link style={{ textDecoration: "none", }} to={renderLink(item.link)} key={index}>
-                                    <ListItem button className={isActive(item.link) ? [classes.item, classes.itemActive] : classes.item}>
-                                        <ListItemIcon>
-                                            {<item.icon className={classes.icon} />}
-                                        </ListItemIcon>
-                                        <ListItemText
-                                            primary={item.name}
-                                            classes={{ primary: classes.text }}
-                                        />
-                                    </ListItem>
-                                </Link>
-                            ))}
-                        </Collapse>
-                        <ListItem button className={classes.item} onClick={() => setMore(!more)}>
-                            <ListItemIcon>
-                                {more ? <ExpandLess className={classes.icon} /> : <ExpandMore className={classes.icon} />}
-                            </ListItemIcon>
-                            <ListItemText
-                                primary={more ? "Thu gọn" : "Xem thêm"}
-                                classes={{ primary: classes.text }}
-                            />
-                        </ListItem>
-                    </>
-                )
-            }
 
         </List>
     )
