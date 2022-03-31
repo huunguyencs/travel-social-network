000b4e11d3e6a86db979e372bd23b6498a29d96f
Your Name <you@example.com>
2022-03-31 23:05:52 +0700

    BE send email

--- server/Routers/user.router.js
+++ server/Routers/user.router.js
@@ -8,21 +8,23 @@ router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 
 router.post("/logout", UserController.logout);
-
-router.post('/refresh_token', UserController.refreshToken);
+router.post("/activate_email",UserController.activateEmail);
+router.post("/refresh_token", UserController.refreshToken);
+router.post("/forgot_password",UserController.forgotPassword);
+router.post("/reset_password",auth, UserController.resetPassword);
 router.patch("/change_avatar", auth, UserController.changeAvatar);
 router.patch("/change_background", auth, UserController.changeBackground);
 router.patch("/change_info", auth, UserController.editProfile);
-router.patch("/change_password", auth, UserController.changePassword)
+router.patch("/change_password", auth, UserController.changePassword);
 
-router.get("/all", auth, authRole([2]), UserController.getAll)
-router.get('/search', UserController.search)
-router.post('/user_list', UserController.getUserByArray)
+router.get("/all", auth, authRole([2]), UserController.getAll);
+router.get('/search', UserController.search);
+router.post('/user_list', UserController.getUserByArray);
 
-router.post("/confirm_account", auth, UserController.confirmAccount)
-router.get('/get_friend_recommend', auth, UserController.getFriendRecommend)
+router.post("/confirm_account", auth, UserController.confirmAccount);
+router.get('/get_friend_recommend', auth, UserController.getFriendRecommend);
 router.get('/search', UserController.searchUsers);
-router.get('/get_tour_saved', auth, UserController.getTourSaved)
+router.get('/get_tour_saved', auth, UserController.getTourSaved);
 
 // lấy thông tin một user
 router.get("/:id", UserController.getUser);
@@ -33,8 +35,8 @@ router.put('/:id/unfollow', auth, UserController.unfollow); // id là id của n
 router.put('/save_tour', auth, UserController.saveTour);
 router.put('/unsaved_tour', auth, UserController.unsaveTour);
 
-router.delete('/delete/:id', auth, authRole([2]), UserController.deleteUser)
-router.patch('/update_status', auth, authRole([2]), UserController.updateStatus)
+router.delete('/delete/:id', auth, authRole([2]), UserController.deleteUser);
+router.patch('/update_status', auth, authRole([2]), UserController.updateStatus);
 
 
 module.exports = router;
