9fa7a0365d2488e7d736e7239d7cac2940d69c08
Huu Nguyen <thieuvan0405@gmail.com>
2022-05-03 22:35:20 +0700

    add google map key

--- src/key/googlemap.js
+++ src/key/googlemap.js
@@ -1,4 +1,4 @@
 // const KEY = "AIzaSyAkBhTU6Tc8FNdu64ZRG4rPm2bin7H7OOI";
 // AIzaSyDcwGyRxRbcNGWOFQVT87A1mkxEOfm8t0w
-const KEY = "";
+const KEY = 'AIzaSyBFPJvoVSICWs_jT95QneW5su4S0kEVcGg';
 export default KEY;
