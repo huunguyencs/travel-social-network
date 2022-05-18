7453a74ca1eb14edf95ecc42bf2487dea7f98a30
Huu Nguyen <thieuvan0405@gmail.com>
2022-05-19 00:46:48 +0700

    fix order tour

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,44 +1,46 @@
 export function getStar(stars) {
-    if (stars && stars.length === 5) {
-        var totalStar = 0, sum = 0;
-        var i = 1;
-        for (var star of stars) {
-            totalStar += i * star;
-            sum += 1 * star;
-            i++;
-        }
-        if (sum === 0) return 0;
-        return Math.floor((totalStar / sum) * 10) / 10;
+  if (stars && stars.length === 5) {
+    var totalStar = 0,
+      sum = 0;
+    var i = 1;
+    for (var star of stars) {
+      totalStar += i * star;
+      sum += 1 * star;
+      i++;
     }
-    return 0;
-
+    if (sum === 0) return 0;
+    return Math.floor((totalStar / sum) * 10) / 10;
+  }
+  return 0;
 }
 
 export function totalNumRate(star) {
-    return star.reduce((a, b) => a + b, 0)
+  return star.reduce((a, b) => a + b, 0);
 }
 
 export function sortTourDate(tour) {
-    return {
-        ...tour,
-        tour: tour.tour.sort((a, b) => a.date > b.date)
-    }
+  return {
+    ...tour,
+    tour: tour.tour.sort((a, b) => new Date(a.date) - new Date(b.date))
+  };
 }
 
 export function extractProvinceTour(tour) {
-    var province = [];
-    tour.map(item => {
-        item.locations.map(location => province.push(location.location.province.fullname))
-        return true;
-    })
-    return new Set(province);
+  var province = [];
+  tour.map(item => {
+    item.locations.map(location =>
+      province.push(location.location.province.fullname)
+    );
+    return true;
+  });
+  return new Set(province);
 }
 
 export function extractLocationTour(location) {
-    var locations = [];
-    location.map(item => {
-        item.locations.map(loc => locations.push(loc.location.fullname))
-        return true;
-    })
-    return new Set(locations);
+  var locations = [];
+  location.map(item => {
+    item.locations.map(loc => locations.push(loc.location.fullname));
+    return true;
+  });
+  return new Set(locations);
 }
